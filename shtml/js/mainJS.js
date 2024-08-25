4b54c197fc105329b00f444c052097e13c52e4e9
Edsome <57480697+Edsome@users.noreply.github.com>
2024-08-25 17:05:46 +0800

    Backup for the actual files

---
+++ shtml/js/mainJS.js
@@ -0,0 +1,468 @@
+var literatureTitle = [
+    "What is Victim Blaming?",
+    "Why people blame victims?",
+    "Just World Hypothesis",
+    "Attribution Error",
+    "Invulnerability Theory",
+    "Victim blaming gender inequality"
+];
+
+var literatureContent = [
+`
+    <h3>What is Victim Blaming?</h3><hr>
+    <span>
+        The definition in Canadian Resource Centre for Victims of Crime (2009) is “Victim blaming is a devaluing act that occurs when the victim(s) of a crime or an accident is held responsible — in whole or in part — for the crimes that have been committed against them".
+        <hr>
+        In other words, victim blaming is about distrust and disbelief in victims, which people generally think that victims are lying or being suspicious in the reporting events, so they call on the prosecution or the public to pay some special attention to victims, instead of the suspect (Elizabeth Conaway, 2017).
+        <hr>
+        Additionally, victim blaming is an action of preserving the interest of the privileged group in power, and justifying abuse or social injustice (William Ryan, 1971; Julia C. Schoellkopf, 2012).
+    </span>
+`,
+
+`
+    <h3>Why do people blame victims?</h3><hr>
+    <span>
+
+    </span>
+`,
+
+`
+    <h3>Just World Hypothesis</h3><hr>
+    <span>
+        People believe that the world is a fair, predictable, and stable environment and that behavioral outcomes are deserved thus allowing us to maintain a sense of control and efficacy over the environment. (Gravelin et al, 2019; Anderson et al, 2001)
+        <hr>
+        That is to say, victim blaming is a way for them to reinforce their values and regard victims as biases by questioning victims why not behaving themselves like the others, so the world will treat them fairly.
+    </span>
+`,
+
+`
+    <h3>Attribution Error</h3><hr>
+    <span>
+        In this point, people believe that the world is logical, where there's smoke, there's fire, rape is also executed in rational.
+        <hr>
+        They perceive the victim as deserving of the misfortune helps to restore the comfortable view of the world as fair and ordered. 
+        <hr>
+        Thus, attributions commenting on the victim's behavior, lack of awareness of potential danger or explicit victim blaming (Anderson et al, 2001).
+    </span>
+`,
+
+`
+    <h3>Invulnerability Theory</h3><hr>
+    <span>
+        This theory asserts that people would like to believe they are different from those victims, they are invulnerable, safe from harmful and gruesome acts against them.
+        <hr>
+        Furthermore, blamers create an idea of "victim fault", it is a kind of using labels to identify behaviors and explain the unfortunate circumstances, such as a rape victim, must have done something to motivate or even invite the rape, which makes victims themselves vulnerable (Conaway, 2017; Gravelin et al, 2019).
+        <hr>
+        Although there are three main theories that researchers try to explain the motivation of victim blaming, it is not independent factors having effect on the individuals. So, it is possible that individuals could fit in those theories at the same time.
+    </span>
+`,
+
+`
+    <h3>Victim blaming gender inequality</h3><hr>
+    <span>
+        In victim blaming circumstances, men attribute greater blame on rape than women who are also significantly influenced by Just World Hypothesis (Caitlin M. Pinciotti & Holly K. Orcutt, 2021; Renata Bongiorno, 2020).
+        <br><br>
+        Furthermore, Renata (2020) indicates that the male-perpetrator empathy is equally or much higher than female-victim empathy.
+        <h3></h3><hr>
+        The reason that having more empathy with the male-perpetrator is to avoid the threat to male dominant society. Male victim blamers are more willing to believe that the male perpetrators did not mean to do it.
+        <br><br>
+        In other words, it has been embedded within culture that women are lesser than men and viewed as subordinated. Women are not allowed to challenge the male dominant world, even if they are be raped, they will be considered as a result of enticement to male. And the male-perpetrators are just doing the things that the female-victims want them to do.
+    </span>
+`
+];
+
+var storyNames = [
+    'Totty Meaghan',
+    'Rylie Roxie',
+    'Yancy Columbine',
+    'Cindi Sadie',
+    'Bindy Lorraine',
+    'Maddy Amelia',
+    'Essence Lisha',
+    'Briley Cherilyn'
+];
+
+var storyWearing = [
+    "Sexy Swimsuit",
+    "Dress Shirt",
+    "Cute mini-dress",
+    "Navy Blue Tank",
+    "Nike Shorts",
+    "Fashion Jean shirt",
+    "Tie Dye Shirt",
+    "Army Combat Uniform"
+];
+
+var storySRC = [
+    "wearing_3",
+    "wearing_4",
+    "wearing_5",
+    "wearing_9",
+    "wearing_11",
+    "wearing_13",
+    "wearing_14",
+    "wearing_15"
+];
+
+var storyContents = [
+    `
+    <h3>A swimsuit</h3><hr>
+    <span>
+        A swimsuit. We had been canoeing at the river all day. It had been a really fun time. Then they came in to my tent when I was trying to change clothes.
+    </span>
+    `,
+    `
+    <h3>The presentation day</h3><hr>
+    <span>
+        Khakis and a dress shirt. I had to give a presentation that day in my communications class. They took my clothes at the hospital during my rape exam. I'm not sure what happened to them.
+    </span>
+    `,
+    `
+    <h3>A mini-dress</h3><hr>
+    <span>
+        A cute mini-dress. I loved it the moment I saw it. I had some killer heels, too. I just wanted to have a good time that night, look cute, and hang with my sisters. He kept getting me shots, over and over again. The next thing I remember is crawling around on the floor looking for that stupid dress.
+    </span>
+    `,
+    `
+    <h3>Blue tank and plaid shorts</h3><hr>
+    <span>
+        Navy blue tank and plaid shorts. Nothing special - no make-up - and sandals. We were just supposed to be hanging out. I didn't think anything about it.
+    </span>
+    `,
+    `
+    <h3>A Concert sweatshirt</h3><hr>
+    <span>
+        Nike shorts and a concert sweatshirt. Seems so normal. So every day. It was too, just any other day, except for this. Except for what happened.
+    </span>
+    `,
+    `
+    <h3>Jeans</h3><hr>
+    <span>
+        Jean shirt, jeans, and Toms. Everyone seems so confused when I tell them this. Like they can't understand what I am saying. They can't understand what I was wearing. It's almost funny. Almost.
+    </span>
+    `,
+    `
+    <h3>She was nice</h3><hr>
+    <span>
+        Jeans and an oversized tie dye shirt. She was like a sister to me. There had never been anything like that between us. Then it was like a switch flipped in her and all of a sudden what I didn't want didn't matter.
+    </span>
+    `,
+    `
+    <h3>Even have a gun</h3><hr>
+    <span>
+        Army Combat Unifor's and I was carrying a gun. So much for that preventing anything.
+    </span>
+    `
+];
+
+var taiwanConditionContent = 
+`
+<h3>The Comments</h3><hr>
+<span>
+    We found that there are lots of phenomena of victim blaming in Taiwan, and it especially happened on the Internet. Below are people's reaction on some cases of rapes from the news in Taiwan. We can see how people reply to the incident under the comment section.
+</span><hr>
+
+<img src="./images/comments/comment_1.png"><br>
+<span>
+    “I am not blaming the victim, but why does she have no self-control? Why did she make herself fuddle? She should have contacted her boyfriend before she got wasted. Besides, her bestie was just next to her? She should end the friendship with this kind of bestie.”
+</span><br><br>
+<span>
+    “This is totally what victim blaming is.”
+</span><br><br>
+<span>
+    “How can you be sure that she didn't mean to do it?”
+</span><br><br><hr>
+
+<img src="./images/comments/comment_2.png"><br>
+<span>
+    “She deserved this as a playgirl. She should behave herself while having a relationship with her boyfriend. Besides, girl hangout with her friend and got wasted, isn't it means okay to others to have sex with her?”
+</span>
+
+<h3>A Small Conclusion</h3><hr>
+<span>
+    From these comments, we can see people blamed victims, and trying to argue the victim made it happened. They rather believe that this happened naturally, because victims did something wrong that leads them to those misfortunes.
+</span><br><br>
+<span>
+    To explain why these phenomenon keep happening, we need take deep dive into the literatures.
+    <!-- they do the wrong thing is to underestimate other people’s aaa this is called fundamental attribution error, as we said in the literature review section, many people take the main cause of rape. Attributable to the personal problems of the victim.For example, "She often walks very close to men", "She often dresses sexy, that's why she will be raped." -->
+</span>
+`;
+
+// input the index of the button
+function popDescription(index) {
+    var x = document.getElementById("description");
+    const body = document.querySelector("body");
+    if (x.className === "stage-items-description-hidden") {
+        body.style.overflow = "hidden";
+        x.className = "stage-items-description";
+        generateDescription(
+            "description-name",
+            storyNames[index],
+            "description-content",
+            storyContents[index]
+        );
+    }
+}
+
+function popAboutUs() {
+    let content = 
+    `
+    <h3>Joanna</h3><hr>
+    <span>Main edit of the project and the document.</span>
+    <h3>Jazz</h3><hr>
+    <span>Help writing the motivation and the solution.</span>
+    <h3>Edsome</h3><hr>
+    <span>The creator of this website.</span>
+    <h3>Rita</h3><hr>
+    <span>Help filtering comments and translate them into English.</span>
+    `;
+    var x = document.getElementById("description");
+    const body = document.querySelector("body");
+    if (x.className === "stage-items-description-hidden") {
+        body.style.overflow = "hidden";
+        x.className = "stage-items-description";
+        generateDescription(
+            "description-name",
+            "About us",
+            "description-content",
+            content
+        );
+    }
+}
+
+function closeDescription() {
+    var x = document.getElementById("description");
+    const body = document.querySelector("body");
+    if (x.className === "stage-items-description") {
+        x.className = "stage-items-description-hidden";
+        body.style.overflowY = "auto";
+    }
+}
+
+function generateDescription(objectIDName, name, objectIDContent, content) {
+    var _name = document.getElementById(objectIDName);
+    var _content = document.getElementById(objectIDContent);
+    _name.innerHTML = name;
+    _content.innerHTML = content;
+}
+
+function initialize() {
+    let itemToAppend = 
+    `
+        <ul class="menu-list unselectable">
+            <li class="menu-for-small-screen" id="title">Victim Blaming</li>
+            <li class="drop-down-items"><a href="stories.html">Stories</a></li>
+            <hr class="drop-down-items" id="separator">
+            <li class="drop-down-items"><a href="literatures.html">Literatures</a></li>
+            <hr class="drop-down-items" id="separator">
+            <li class="drop-down-items"><a href="motivation.html">Motivation</a></li>
+            <hr class="drop-down-items" id="separator">
+            <li class="drop-down-items"><a href="solutions.html">Solutions</a></li>
+            
+            <hr class="drop-down-items" id="separator">
+            <li class="menu-for-small-screen">
+                <a class="icon" onclick="changeViewMode()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </li>
+        </ul>
+    `;
+    var target = document.getElementById("theIcon");
+    target.innerHTML = itemToAppend;
+
+    itemToAppend = 
+    `
+    <hr>
+    <a href="./files/Victim_blaming_on_rape.pdf" download>
+        <i class="fa-solid fa-download"></i>
+        Full Document
+    </a>
+    <hr>
+    <a onclick="popAboutUs()">
+        <i class="fa-solid fa-circle-info"></i>
+        About us
+    </a>
+    <hr>
+    `;
+
+    target = document.getElementById("theFooter");
+    target.innerHTML = itemToAppend;
+}
+
+// replace the innerHTML of each element with the class name you input
+function addText(objectClass, text) {
+    $(objectClass).each(function(){
+        $(this).html(text);
+    })
+}
+
+// for responsive deign use, the sidebar
+function changeViewMode() {
+    var x = document.getElementById("theIcon");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
+
+// for stories page use, it pops
+function popNotification() {
+    var x = document.getElementById("notification");
+    if (x.className === "notification") {
+        x.className = "notification-hidden";
+        setTimeout(notification_in, 4200);
+    } else {
+        generateComment();
+        x.className = "notification";
+        setTimeout(notification_out, 4200);
+    }
+}
+
+function notification_in() {
+    var x = document.getElementById("notification");
+    x.className = "notification";
+}
+
+function notification_out() {
+    var x = document.getElementById("notification");
+    x.className = "notification-hidden";
+}
+
+// generate literature content for the button clicked
+function generateLiteratureContent(index) {
+    // console.log(index);
+    var target = document.getElementById("description");
+    var _title = document.getElementById("description-name")
+    var _content = document.getElementById("description-content");
+    const body = document.querySelector("body");
+    if (target.className === "stage-items-description-hidden") {
+        _content.innerHTML = literatureContent[index];
+        _title.innerHTML = "Literatures";
+        // _title.innerHTML = literatureTitle[index];
+        target.className = "stage-items-description";
+        body.style.overflow = "hidden";
+    } else {
+        target.className = "stage-items-description-hidden";
+        body.style.overflow = "auto";
+    }
+}
+
+function displayTaiwanCondition() {
+    var target = document.getElementById("description");
+    var _title = document.getElementById("description-name")
+    var _content = document.getElementById("description-content");
+    if (target.className === "stage-items-description-hidden") {
+        _content.innerHTML = taiwanConditionContent;
+        _title.innerHTML = "Taiwan's Current Condition";
+        target.className = "stage-items-description";
+    } else {
+        target.className = "stage-items-description-hidden";
+    }
+}
+
+// randomly generate comments
+function generateComment() {
+    let userPicture = [
+        'user_1.jpg',
+        'user_2.jpg',
+        'user_3.jpg',
+        'user_4.jpg',
+        'user_5.jpg',
+        'user_6.jpg',
+        'user_7.jpg',
+        'user_8.jpg',
+        'user_9.jpg'
+    ];
+    let userName    = [
+        'Johnny Wick',
+        'Ansley Leigh',
+        'Jinping Xi',
+        'Brandon Oliver',
+        'Ashton Phoenix',
+        'Alora Pearl',
+        'Black Scott',
+        'Patricho Liu',
+        'Arlo James'
+    ];
+    let userComment = [
+        'Did you scream?',
+        'Did you provoke him?',
+        'What were you wearing?' ,
+        'You are raped because you dressed like that!',
+        'You made them wanted.',
+        'Why don\'t you shout it out?',
+        'Why you didn\'t say no?',
+        'You are such a whore doing this.',
+        'I don\'t believe you.'
+    ];
+
+    let index = getRandomInt(Math.min(userPicture.length, userName.length, userComment.length));
+    var _picture    = document.getElementById("notification-image");
+    var _name       = document.getElementById("notification-username");
+    var _comment    = document.getElementById("notification-text");
+    _picture.src        = "./images/userPictures/" + userPicture[index];
+    _name.innerHTML     = userName[index];
+    _comment.innerHTML  = userComment[index];
+}
+
+// possibility is 0 ~ 1, floating number
+function rollDie(possibility) {
+    if (possibility < 0 || possibility > 1) {
+        console.log("rollDie() returns false(0)");
+        return false;
+    }
+
+    possibility *= 100;
+    if (getRandomInt(100) <= possibility) {
+        console.log("rollDie() returns true");
+        return true;
+    } else {
+        console.log("rollDie() returns false(1)");
+        return false;
+    }
+}
+
+// returns 0-based index for arrays
+// NO possibility to get the number you input
+// MAX = your input - 1
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function popMoreMoti() {
+    let content =
+    `
+    <h3>The Taiwan Situation</h3><hr>
+    <span class="text">
+        Unfortunately, this is a phenomenon that is not uncommon to occur in our society.
+    </span><br>
+    <span class="text">
+        From law enforcement officers to news reports, and also quite typical in comments under social media posts.
+    </span><br>
+    <span class="text">
+        Every now and then, when scrolling through Facebook and Instagram posts, we see posts related to sexual harassment or rape, and under these posts, we often see people commenting on how it is the victim's behaviors that resulted in the event.
+    </span><br>
+    <h3>It's not easy</h3><hr>
+    <span class="text">
+        The cause of these incidents should be rather clear and straightforward, but it does not seem to be the case from our observations. Going through these horrible experiences is unpleasant in the first place, having to face additional mistaken blaming is unnecessary and more importantly, is quite heartbreaking.
+    </span><br>
+    <h3>The Goal</h3><hr>
+    <span class="text">
+        Thus, we decided to investigate the issue and hopefully come up with measures that can ultimately stop the forming of wrongly blamed victims.
+    </span>
+    `;
+
+    var x = document.getElementById("description");
+    const body = document.querySelector("body");
+    if (x.className === "stage-items-description-hidden") {
+        body.style.overflow = "hidden";
+        x.className = "stage-items-description";
+        generateDescription(
+            "description-name",
+            "More about our motivation",
+            "description-content",
+            content
+        );
+    }
+}
